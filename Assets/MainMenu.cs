87a4094f94eaf2ccf0d9e27ea5f5f9cce2e804ff
Matt Radbourne <m@radbourne.me>
2020-02-01 20:04:49 +0000

    Ad PARALLAX to title screen

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject blokeyFrame;
+    public GameObject gravestoneFrame;
+    public GameObject backgroundFrame;
+
     private void Update()
     {
+        this.UpdateParallax();
+
         if (Input.GetKey(KeyCode.Return))
         {
             this.PlayGame();
@@ -20,5 +28,19 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit executed");
         Application.Quit();
+    }
+
+    private void UpdateParallax()
+    {
+        var mouseX = (Input.mousePosition.x * 2) / Screen.width;
+        Debug.Log(mouseX);
+        var pos = blokeyFrame.transform.position;
+        blokeyFrame.transform.position = new Vector3(mouseX * 10, pos.y, pos.z);
+
+        pos = gravestoneFrame.transform.position;
+        gravestoneFrame.transform.position = new Vector3( mouseX * 20, pos.y, pos.z);
+
+        pos = backgroundFrame.transform.position;
+        backgroundFrame.transform.position = new Vector3(mouseX * 40, pos.y, pos.z);
     }
 }
