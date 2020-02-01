3401a115eee9298268696417559f8e47bd36c748
CarolineClark <5319573+CarolineClark@users.noreply.github.com>
2020-02-01 12:39:39 +0000

    Mind fuck

---
+++ Assets/Face.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    public int faceNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Map.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Face[] _faces;
+    private Face _currentFace;
+    private Face _face0;
+    private Face _face1;
+    private Face _face2;
+    private Face _face3;
+    private Face _face4;
+    private Face _face5;
+
+    void Start()
+    {
+        _faces = GetComponentsInChildren<Face>();
+        _faces.Where(face => face.faceNumber == 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
