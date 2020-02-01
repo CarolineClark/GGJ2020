7881cee4b796abc70d42dd0aae4f2b35e4c9cf20
Matt Radbourne <m@radbourne.me>
2020-02-01 18:28:40 +0000

    Update title screen bg image and allow game to be started with Enter

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            this.PlayGame();
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Scenes/SampleScene");
