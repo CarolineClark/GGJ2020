90a26b44f1f88b8ea24f91e9a54d3bf9b78ddc7e
Matt Radbourne <m@radbourne.me>
2020-02-01 22:24:02 +0000

    Add dat player to da scene

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject startingTile;
+    public GameObject playerGraphicObject;
+
+    private void Start()
+    {
+        var pos = startingTile.transform.position;
+        transform.position = new Vector3(pos.x, 0.75f, pos.z);
+        playerGraphicObject.transform.rotation = Quaternion.Euler(0, -135, 0);
+    }
+}
