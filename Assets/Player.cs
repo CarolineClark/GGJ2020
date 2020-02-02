92ca8522fb3a56a37e60a17e01946cc1b17cb4e6
CarolineClark <5319573+CarolineClark@users.noreply.github.com>
2020-02-02 13:30:26 +0000

    Adding player animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     public GameObject startingTile;
     public GameObject playerGraphicObject;
+    private Animator _animator;
 
     private void Start()
     {
         var pos = startingTile.transform.position;
         transform.position = new Vector3(pos.x, 0.75f, pos.z);
         playerGraphicObject.transform.rotation = Quaternion.Euler(0, -135, 0);
+        _animator = GetComponent<Animator>();
     }
 }
