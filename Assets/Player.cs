e79c2a13098c4c5626cc71f1fce63ab114487638
adrien.baron <adrien.baron@cazoo.co.uk>
2020-02-02 13:44:07 +0000

    Make maps separate on portal

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject startingTile;
     public GameObject playerGraphicObject;
 
     private void Start()
     {
-        var pos = startingTile.transform.position;
-        transform.position = new Vector3(pos.x, 0.75f, pos.z);
+    }
+
+    public void SetTile(GameObject tile)
+    {
+        var pos = tile.transform.position;
+        transform.position = new Vector3(pos.x + 1f, pos.y + 0.75f, pos.z);
         playerGraphicObject.transform.rotation = Quaternion.Euler(0, -135, 0);
     }
 }
