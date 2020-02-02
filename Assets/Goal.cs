b8c3511e6cbdb34944fa07ee33b6c165f309363d
Ryan Loader <ryan.loader@theappbusiness.com>
2020-02-02 12:30:18 +0000

    Transition between tutorial and sample scenes

---
+++ Assets/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public string TargetScene;
+    private bool playerAtGoal = false;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("OnTriggerEnter");
+        playerAtGoal = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("OnTriggerExit");
+        playerAtGoal = false;
+    }
+
+    public void Interact() {
+        if (playerAtGoal) {
+            Debug.Log("Player at goal, changing scenes: " + TargetScene);
+            SceneManager.LoadScene(TargetScene);
+        } else {
+            Debug.Log("Player not at goal, interaction ignored");
+        }
+    }
+}
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -1,10 +1,12 @@
 public class PlayerInput
 {
-    public PlayerInput(bool Left, bool Right)
+    public PlayerInput(bool Left, bool Right, bool Interact)
     {
         this.Left = Left;
         this.Right = Right;
+        this.Interact = Interact;
     }
     public bool Left { get; private set; }
     public bool Right { get; private set; }
+    public bool Interact { get; private set; }
 }
